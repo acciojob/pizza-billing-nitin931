9dc819809473e9f78162c0fb8e28f2062465fddd
nitin931 <nitinthakur931@gmail.com>
2023-09-05 19:24:25 +0530

    Assignment

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,40 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+
+        super.addExtraCheese();
+        super.addExtraToppings();
+    }
+
+    @Override
+    public String getBill() {
+        String bill;
+        if(super.getVeg()){
+            if(getValidBag()){
+                bill = "Veg-Deluxe Pizza Price : "+getPrice();
+            }
+            else {
+                bill = "Veg-Deluxe Pizza Prize : "+(getPrice()-20) +"\nPaper bag Price : 20";
+            }
+        }
+        else{
+            if(getValidBag()){
+                bill = "Non-veg-Deluxe Pizza Price : "+getPrice();
+            }
+            else {
+                bill = "Non-veg-Deluxe Pizza Prize : "+(getPrice()-20) +"\nPaper bag Price : 20";
+            }
+
+        }
+        return bill +"\nTotal Price : "+getPrice();
+    }
+
+    @Override
+    public void addExtraCheese() {
+
+    }
+    @Override
+    public void addExtraToppings() {
+        super.addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,60 @@ package com.driver;
 
 public class Pizza {
 
+    private Boolean validBag;
+    private Boolean validCheese;
+    private Boolean validToppings;
     private int price;
-    private Boolean isVeg;
+    private final Boolean isVeg;
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        validCheese = true;
+        validToppings = true;
+        validBag = true;
+        if(isVeg){
+            this.price = 300;
+            bill = "Veg pizza base price : 300\n";
+        }
+        else{
+            this.price=400;
+            bill = "Non-veg pizza base price : 400\n";
+        }
+
         // your code goes here
+    }
+
+    public Boolean getVeg() {
+        return isVeg;
+    }
+
+    public Boolean getValidBag() {
+        return validBag;
+    }
+
+    public void setValidBag(Boolean validBag) {
+        this.validBag = validBag;
+    }
+
+    public Boolean getValidCheese() {
+        return validCheese;
+    }
+
+    public void setValidCheese(Boolean validCheese) {
+        this.validCheese = validCheese;
+    }
+
+    public Boolean getValidToppings() {
+        return validToppings;
+    }
+
+    public void setValidToppings(Boolean validToppings) {
+        this.validToppings = validToppings;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     public int getPrice(){
@@ -17,18 +64,40 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(getValidCheese()){
+            bill += "Extra Cheese Price : 80\n";
+            price += 80;
+            setValidCheese(false);
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(getValidToppings()){
+            if(isVeg){
+                bill += "Extra Toppings For Veg Pizza : 70\n";
+                price += 70;
+            }
+            else{
+                bill += "Extra Toppings For Non-veg Pizza : 120\n";
+                price += 120;
+            }
+            setValidToppings(false);
+        }
+
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(getValidBag()){
+            bill += "Paper bag Price : 20\n";
+            price += 20;
+        }
+        setValidBag(false);
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return this.bill + "Total : "+price;
     }
 }
